f2bf87906883c0fd6f761c741da1168886c786b0
ltxhhz <ltxhhz@qq.com>
2023-12-16 12:51:40 +0800

    12.16

--- readme.md
+++ readme.md
@@ -3,3 +3,19 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-give-you-some-color?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-give-you-some-color)
 
 给你点颜色看看
+
+使用了 [color - npm (npmjs.com)](https://www.npmjs.com/package/color) 库，输入的字符串由 [color-string - npm (npmjs.com)](https://www.npmjs.com/package/color-string) 库处理。
+
+支持部分中文颜色，因为和英文[做了映射](./src/colors.ts)。
+
+## 前置
+
+- [ltxhhz/koishi-plugin-skia-canvas: 基于 skia-canvas 提供 koishi 的 skia 服务 (github.com)](https://github.com/ltxhhz/koishi-plugin-skia-canvas)
+
+## 示例
+
+![示例](./images/1.png "示例")
+
+## 其他插件
+
+* [ltxhhz/koishi-plugin-imagify-skia: koishi 插件，使用 skia-canvas 的图形化输出，性能优于 puppeteer (github.com)](https://github.com/ltxhhz/koishi-plugin-imagify-skia)
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,53 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
+
+import type {} from '@ltxhhz/koishi-plugin-skia-canvas'
+import Color from 'color'
+import colors from './colors'
 
 export const name = 'give-you-some-color'
 
-export interface Config {}
+export const inject = ['skia']
 
-export const Config: Schema<Config> = Schema.object({})
-
-export function apply(ctx: Context) {
-  // write your plugin here
+export interface Config {
+  width?: number
+  height?: number
 }
+
+export const Config: Schema<Config> = Schema.object({
+  width: Schema.number().default(100).description('图片宽'),
+  height: Schema.number().default(100).description('图片高')
+})
+
+export function apply(ctx: Context, config: Config) {
+  const logger = ctx.logger('color')
+  ctx
+    .command('color [color:string] 给你点颜色看看')
+    .alias('颜色', '给你点颜色看看')
+    .shortcut(/^这是\s*(.+)\s*色$/, { args: ['$1'] })
+    .shortcut(/^这是\s*(#.{3,8})\s*$/, { args: ['$1'] })
+    .shortcut(/^这是\s*(\w+?)\((.+)\)\s*$/, { args: ['$1($2)'] })
+    .action(({}, ...args) => {
+      let color = args.join('')
+      const { Canvas } = ctx.skia
+      logger.info(color)
+      if (color) {
+        try {
+          const colorName = colors[color]
+          if (colorName) {
+            color = colorName
+          } else {
+            color = Color(color).hexa()
+          }
+          logger.info(color)
+          const canvas = new Canvas(config.width, config.height)
+          const cCtx = canvas.getContext('2d')
+          cCtx.fillStyle = color
+          cCtx.fillRect(0, 0, canvas.width, canvas.height)
+          return h.image(canvas.toBufferSync('png'), 'image/png')
+        } catch (error) {
+          logger.error(error)
+        }
+      }
+    })
+}
+
